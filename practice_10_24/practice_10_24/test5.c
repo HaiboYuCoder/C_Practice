00063ed2686ff748bd04740e1326355076e83c82
HaiboYuCoder <3218541421@qq.com>
2022-10-25 16:12:53 +0800

    1

---
+++ practice_10_24/practice_10_24/test5.c
@@ -0,0 +1,24 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+/*
+int main()
+{
+	int i = 1;
+	float b = 0.0f;
+	float sum = 0.0f;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 2 == 0)
+		{
+			b = -1.0f / i;
+		}
+		else
+		{
+			b = 1.0f / i;
+		}
+		sum = b + sum;
+	}
+	printf("%f\n", sum);
+	return 0;
+}*/
