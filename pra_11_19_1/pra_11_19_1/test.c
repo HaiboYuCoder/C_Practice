ab935251c428db7e64911dbb68b1e549ec7d7df6
HaiboYuCoder <3218541421@qq.com>
2022-11-19 15:56:03 +0800

    判断是不是字母

---
+++ pra_11_19_1/pra_11_19_1/test.c
@@ -0,0 +1,21 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	char a = 0;
+	while (scanf("%c", &a) != EOF)
+	{
+		getchar();
+		//printf("%d\n", a);
+		if (a >= 65 && a <= 90 || a >= 97 && a <= 122)
+		{
+			printf("%c is an alphabet.\n", a);
+		}
+		else
+		{
+			printf("%c is not an alphabet.\n", a);
+		}
+	}
+	return 0;
+}
